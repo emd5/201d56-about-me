f8a1dcc0e8980f047d8e36a6e1ca0bb58dd674eb
Roger Huba <rogerhuba@gmail.com>
2019-03-27 16:39:35 -0700

    working up to question 6

--- js/quiz.js
+++ js/quiz.js
@@ -26,19 +26,24 @@ const rndAns = ['yes', 'no', 'yes', 'yes', 'no', 'no', 'yes', 'yes', 'yes', 'no'
 // Array to track displayed questions so duplicates do not get put on the screen.
 let questUsed = ['no','no','no','no','no','no','no','no','no','no','no','no','no','no','no','no'];
 
+// Array for Question 7.  What is Rogers Highest Level of Education?
 let userAnswer = [];
 let randomQuestion = '';
 let totalRight = 0;
 let numberOfQuestions = 2;
 let gameAnswer = Math.floor(Math.random() * 101);
-let numberOfGuesses;
-let guessChances = 7;
+let numberOfGuesses = 0;
+let guessChances = 4;
 let userGuess = 0;
+let guessString = ' ';
+let guessChancesLeft = guessChances;
+let guessMessage = 'Lets Play a quick Guessing Game.';
+let confirmButton = false;
+
 
 let userName = prompt('Please enter your name');
 
 for (let i = 0; i < numberOfQuestions; i++){
-  console.log('Loop number is: ' + i);
   randomQuestion = Math.floor(Math.random() * (rndQues.length));
   console.log('Random Number: ' + randomQuestion);
 
@@ -51,18 +56,53 @@ for (let i = 0; i < numberOfQuestions; i++){
 
   if (userAnswer[i].charAt(0) === rndAns[randomQuestion].charAt(0)){
     totalRight += 1;
+    console.log('Total Right Answers: ' + totalRight);
     alert('You got that one right ' + userName + '!  Nice work.');
   } else {
     alert('Sorry ' + userName + '.  Feels Bad Man!');
   }
 }
 
-while (userGuess !== gameAnswer || numberOfGuesses < guessChances) {
-  userGuess = prompt('Lets Play a quick Guessing Game. ' + guessChances + ' left!');
-  if (userGuess !== gameAnswer && guessChances < 1) {
-    
+while (userGuess !== gameAnswer && numberOfGuesses < guessChances) {
+  userGuess = prompt(guessMessage + '\nNumbers Guessed so far: ' + guessString + ' ' + '\nPick a number ' +
+    'between 1 and 100. (' + guessChancesLeft + ') more chances left!');
+  numberOfGuesses += 1;
+  console.log('UserGuess: ' + userGuess + '. GameAnswer: ' + gameAnswer + '. Number of Guesses: ' + numberOfGuesses);
+
+  guessString += userGuess + ' ';
+
+  if (parseInt(userGuess) === gameAnswer) {
+    totalRight += 1;
+    console.log('Total Right Answers: ' + totalRight);
+    confirmButton = confirm('DING DING DING.  You got it.  Great work. On to the last question!');
+    while (confirmButton === false) {
+      if (confirmButton) {
+        break;
+      } else {
+        confirmButton = confirm('Please Press Ok to continue');
+      }
+    }
+    break;
+  } else if (userGuess > gameAnswer) {
+    guessMessage = 'Try Again.  This time Lower';
+    guessChancesLeft -= 1;
+  } else if (userGuess < gameAnswer) {
+    guessMessage = 'Try Again.  This time Higher';
+    guessChancesLeft -= 1;
+  }
+}
+
+if ( guessChancesLeft < 1) {
+  confirm('Sorry, all out of guesses. The correct answer was: ' + gameAnswer + '. On to the last question');
+  numberOfQuestions =+ 1;
+  while (confirmButton === false) {
+    if (confirmButton) {
+      break;
+    } else {
+      confirmButton = confirm('Please Press Ok to continue to the last question.');
+    }
   }
 }
 
 
-alert('You got ' + totalRight + ' answers correct out of ' + numberOfQuestions + '.')
+alert('You got ' + totalRight + ' answers correct');
